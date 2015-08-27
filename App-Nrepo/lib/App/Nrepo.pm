1b7f76ddea0b39a04b1fe03896f39b952761ed8b
Brendan Beveridge <brendan.beveridge@optus.com.au>
2015-08-27 11:22:17 +1000

    working out initials

--- App-Nrepo/lib/App/Nrepo.pm
+++ App-Nrepo/lib/App/Nrepo.pm
@@ -1,6 +1,39 @@
-use strict;
-use warnings;
 package App::Nrepo;
+
+use strict;
+use warnings FATAL => 'all';
+use Moo;
+use Carp;
+use namespace::clean;
+use Params::Validate qw(:all);
+use Data::Dumper;
+
+has config => ( is => 'ro' );
+has logger => ( is => 'ro' );
+
+sub go {
+  my $self = shift;
+  my $options = shift;
+
+  $self->_validate_config();
+  #$self->_validate_options($options);
+  #if ($options->{'list'}) {
+
+
+}
+sub _validate_config {
+  my $self = shift;
+
+  $self->logger->log_and_croak(
+    level   => 'error',
+    message => sprintf "DataDir does not exist: %s", $self->config->{DataDir},
+  ) unless -d $self->config->{DataDir};
+
+  $self->logger->log_and_croak(
+    level   => 'error',
+    message => sprintf "Unknown TagStyle %s, must be TopDir or BottomDir\n", $self->config->{TagStyle},
+  ) unless $self->config->{TagStyle} =~ m/^(?:Top|Bottom)Dir$/;
+}
 
 1;
 __END__
