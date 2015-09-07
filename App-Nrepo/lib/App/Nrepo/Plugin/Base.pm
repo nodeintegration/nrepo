a9b06046f59d32046886f15cd27806005a3ec6e4
Brendan Beveridge <brendan.beveridge@optus.com.au>
2015-09-07 13:56:17 +1000

    change plugin structure, more work on clean action

---
+++ App-Nrepo/lib/App/Nrepo/Plugin/Base.pm
@@ -0,0 +1,178 @@
+package App::Nrepo::Plugin::Base;
+
+use Carp;
+use Data::Dumper;
+use Digest::SHA;
+use File::Path qw(make_path);
+use LWP::UserAgent;
+use Moo::Role;
+use Module::Path qw[ module_path ];
+use Module::Runtime qw[ compose_module_name ];
+use namespace::clean;
+use Params::Validate qw(:all);
+use Time::HiRes qw(gettimeofday tv_interval);
+
+has logger    => ( is => 'ro', required => 1 );
+has repo      => ( is => 'ro', required => 1 );
+has dir       => ( is => 'ro', required => 1 );
+has url       => ( is => 'ro', optional => 1 );
+has checksums => ( is => 'ro', optional => 1 );
+has force     => ( is => 'ro', optional => 1 );
+has arches    => ( is => 'ro', required => 1 );
+has ua        => ( is => 'lazy' );
+has ssl_ca    => ( is => 'ro', optional => 1 );
+has ssl_cert  => ( is => 'ro', optional => 1 );
+has ssl_key   => ( is => 'ro', optional => 1 );
+
+sub _build_ua {
+  my $self = shift;
+
+  my %o;
+  $o{ssl_opts}->{'SSL_ca_file'}   = $self->ssl_ca()   if $self->can('ssl_ca');
+  $o{ssl_opts}->{'SSL_cert_file'} = $self->ssl_cert() if $self->can('ssl_cert');
+  $o{ssl_opts}->{'SSL_key_file'}  = $self->ssl_key()  if $self->can('ssl_key');
+
+  return LWP::UserAgent->new(%o);
+}
+
+sub get_gzip_contents {
+  my $self = shift;
+  my $file = shift;
+
+  if (-f $file) {
+    {
+        my $fh = IO::Zlib->new($file, 'rb');
+
+        #XXX for some reason this does not work
+        #local $/ = undef;
+        #my $contents = <$fh>;
+        #return $contents;
+
+        my @contents = <$fh>;
+        $fh->close;
+        return join('', @contents);
+    }
+  }
+}
+
+sub make_dir {
+  my $self = shift;
+  my $dir  = shift;
+  if (! -d $dir) {
+    my $err;
+    my $dirs = make_path($dir, error => \$err);
+    $self->logger->log_and_croak(level => 'error', message => "Failed to create path: ${dir} with error: ${err}") if $err;
+    $self->logger->debug("Created path: ${dir}");
+    return 1;
+  }
+  return 0;
+}
+
+sub validate_file {
+  my $self = shift;
+  my %o = validate(@_, {
+    filename => { type => SCALAR },
+    check    => { type => SCALAR },
+    value    => { type => SCALAR },
+  });
+
+  return 0 unless -f $o{'filename'};
+
+  if ($o{'check'} eq 'size') {
+    return $self->_validate_file_size($o{'filename'}, $o{'value'});
+  }
+  elsif ($o{'check'} eq 'sha256') {
+    return $self->_validate_file_sha256($o{'filename'}, $o{'value'});
+  }
+}
+
+sub _validate_file_size {
+  my $self = shift;
+  my $file = shift;
+  my $size = shift;
+
+  my @stats     = stat($file);
+  my $file_size = $stats[7];
+
+  return $file_size eq $size ? 1 : undef;
+}
+
+sub _validate_file_sha256 {
+  my $self     = shift;
+  my $file     = shift;
+  my $checksum = shift;
+
+  my $sha = Digest::SHA->new('sha256');
+  $sha->addfile($file);
+  return $sha->hexdigest eq $checksum ? 1 : undef;
+}
+
+sub mirror {
+  my $self = shift;
+
+  $self->logger->debug(sprintf("mirror: starting repo: %s from url: %s to dir: %s", $self->repo, $self->url, $self->dir));
+
+  for my $arch (@{$self->arches()}) {
+    my $packages = $self->get_metadata($arch);
+    $self->get_packages(arch => $arch, packages => $packages);
+  }
+
+}
+sub clean {
+  my $self = shift;
+
+  $self->logger->debug(sprintf("clean: starting repo: %s in dir: %s", $self->repo, $self->dir));
+  for my $arch (@{$self->arches()}) {
+    my $files = $self->read_metadata($arch);
+    $self->clean_files(arch => $arch, files => $files);
+  }
+
+}
+
+#XXX TODO
+sub init {
+  my $self = shift;
+  #$plugin->init();
+  print "Here\n";
+
+}
+
+sub download_binary_file {
+  my $self = shift;
+  my %o = validate(@_, {
+    url         => { type => SCALAR },
+    dest        => { type => SCALAR },
+    retry_limit => { type => SCALAR, default => 3 },
+  });
+
+  $self->logger->debug("download_binary_file: $o{url} -> $o{dest}");
+
+  my $retry_count = 0;
+  my $retry_limit = $o{retry_limit};
+  my $success;
+
+  while (!$success && $retry_count <= $retry_limit) {
+    my $t0 = [gettimeofday];
+    my $res = $self->ua->get($o{'url'}, ':content_file' => $o{'dest'});
+    my $elapsed = tv_interval($t0);
+
+    $self->logger->debug("download_binary_file: $o{url} took: ${elapsed}");
+
+    if ($res->is_success) {
+      return 1;
+    }
+    else {
+      $self->logger->debug("download_binary_file: $o{url} failed with status: " . $res->status_line);
+      $retry_count++;
+      if ($retry_count <= $retry_limit) {
+        $self->logger->debug("download_binary_file: $o{url} retrying") if $retry_count;
+      }
+      else {
+        $self->logger->error("download_binary_file: $o{url} failed and exhausted all retries.");
+        return undef;
+      }
+    }
+  }
+}
+
+1;
---
+++ App-Nrepo/lib/App/Nrepo/Plugin/Yum.pm
@@ -0,0 +1,247 @@
+package App::Nrepo::Plugin::Yum;
+
+use Moo;
+use Carp;
+use IO::Zlib;
+use File::Find qw(find);
+use File::Basename qw(dirname);
+use Params::Validate qw(:all);
+use Data::Dumper qw(Dumper);
+use XML::Twig;
+
+with 'App::Nrepo::Plugin::Base';
+
+sub get_metadata {
+  my $self = shift;
+  my $arch = shift;
+
+  my $base_dir = File::Spec->catdir($self->dir(), $arch);
+  my $packages;
+
+  my @metadata_files = ({type => 'repomd', location => 'repodata/repomd.xml'});
+  for my $m (@metadata_files) {
+    my $type      = $m->{'type'};
+    my $location  = $m->{'location'};
+    my $m_url     = join('/', ($self->url, $location));
+    $m_url        =~ s/%ARCH%/$arch/;
+    my $dest_file = File::Spec->catfile($base_dir, $location);
+    my $dest_dir  = dirname($dest_file);
+
+    # Make sure dir exists
+    $self->make_dir($dest_dir);
+
+    # Grab the file
+    $self->download_binary_file(url => $m_url, dest => $dest_file);
+
+    # Parse the xml and retrieve the primary file location
+    if ($type eq 'repomd') {
+      my $data = $self->parse_repomd($dest_file);
+      push @metadata_files, @{$data};
+    }
+
+    # Parse the primary metadata file
+    if ($type eq 'primary') {
+      my $contents = $self->get_gzip_contents($dest_file);
+      $packages = $self->parse_primary($contents);
+    }
+  }
+  return $packages;
+}
+
+sub read_metadata {
+  my $self = shift;
+  my $arch = shift;
+
+  my $base_dir = File::Spec->catdir($self->dir(), $arch);
+  my $files = {};
+
+  my @metadata_files = ({type => 'repomd', location => 'repodata/repomd.xml'});
+  for my $m (@metadata_files) {
+    my $type      = $m->{'type'};
+    my $location  = $m->{'location'};
+    my $dest_file = File::Spec->catfile($base_dir, $location);
+    my $dest_dir  = dirname($dest_file);
+
+    $files->{$location}++;
+
+    if (-f $dest_file) {
+      # Parse the xml and retrieve the primary file location
+      if ($type eq 'repomd') {
+        my $data = $self->parse_repomd($dest_file);
+        push @metadata_files, @{$data};
+      }
+
+      # Parse the primary metadata file
+      if ($type eq 'primary') {
+        my $contents = $self->get_gzip_contents($dest_file);
+        for my $file (@{$self->parse_primary($contents)}) {
+          $files->{$file->{'location'}}++;
+        }
+      }
+    }
+  }
+  return $files;
+}
+
+sub parse_repomd {
+  my $self = shift;
+  my $file = shift;
+
+  my $twig = XML::Twig->new(TwigRoots => {data => 1});
+  $twig->parsefile($file);
+
+  my $root = $twig->root;
+  my @e = $root->children();
+  my @files;
+  for my $e (@e) {
+    my $data = {};
+    $data->{'type'} = $e->att('type');
+    for my $c ($e->children()) {
+      if ($c->name eq 'location') {
+        $data->{'location'} = $c->att('href');
+      }
+      elsif ($c->name eq 'checksum') {
+        $data->{'checksum'} = $c->att('type');
+      }
+      elsif ($c->name eq 'size'){
+        $data->{'size'} = $c->text;
+      }
+    }
+    $self->logger->log_and_croak(level => 'error', message => "repomd xml not valid: $file") unless $data->{'location'};
+    push @files, $data;
+  }
+
+  return \@files;
+}
+sub parse_primary {
+  my $self = shift;
+  my $xml  = shift;
+
+  my $packages = [];
+  my $twig = XML::Twig->new(TwigRoots => {package => 1});
+  $twig->parse($xml);
+  my $root = $twig->root;
+  my @e = $root->children();
+  for my $e (@e) {
+    my $data = {};
+    for my $c ($e->children()) {
+      if ($c->name eq 'location') {
+        $data->{'location'} = $c->att('href');
+      }
+      elsif ($c->name eq 'name') {
+        $data->{'name'} = $c->text;
+      }
+      elsif ($c->name eq 'checksum') {
+        $data->{'checksum'}->{'type'} = $c->att('type');
+        $data->{'checksum'}->{'value'} = $c->text;
+      }
+      elsif ($c->name eq 'size'){
+        $data->{'size'} = $c->att('package');
+      }
+    }
+    push @{$packages}, $data;
+  }
+  return $packages;
+}
+sub get_packages {
+  my $self = shift;
+  my %o = validate(@_, {
+    arch     => { type => SCALAR },
+    packages => { type => ARRAYREF },
+  });
+
+  my $arch     = $o{'arch'};
+  my $base_dir = File::Spec->catdir($self->dir(), $arch);
+
+  for my $package (@{$o{'packages'}}) {
+    my $name     = $package->{'name'};
+    my $size     = $package->{'size'};
+    my $location = $package->{'location'};
+    my $checksum = $package->{'checksum'};
+
+    my $p_url     = join('/', ($self->url, $location));
+    $p_url        =~ s/%ARCH%/$arch/;
+    my $dest_file = File::Spec->catfile($base_dir, $location);
+    my $dest_dir  = dirname($dest_file);
+
+    # Make sure dir exists
+    $self->make_dir($dest_dir);
+
+    # Check if we have the local file
+    my $download;
+    if ($self->force) {
+      $download++;
+    }
+    elsif ($self->checksums) {
+      $download++ unless $self->validate_file(filename => $dest_file, check => $checksum->{'type'}, value => $checksum->{'value'});
+    }
+    else {
+      $download++ unless $self->validate_file(filename => $dest_file, check => 'size', value => $size);
+    }
+
+    # Grab the file
+    if ($download) {
+      $self->download_binary_file(url => $p_url, dest => $dest_file);
+    }
+    else {
+      $self->logger->debug("get_packages: skipping package: ${name} as its deemed up to date");
+    }
+  }
+}
+sub clean_files {
+  my $self = shift;
+  my %o = validate(@_, {
+    arch  => { type => SCALAR },
+    files => { type => HASHREF },
+  });
+
+  my $arch     = $o{'arch'};
+  my $base_dir = File::Spec->catdir($self->dir(), $arch);
+
+  my $full_files = {};
+  for my $file (keys %{$o{'files'}}) {
+    $full_files->{File::Spec->catdir($base_dir, $file)}++;
+  }
+  find(sub {
+    if ($_ !~ /^[\.]+$/ and ! -d $_) {
+      #print "found: $_!\n"
+      unless ($full_files->{$File::Find::name}) {
+        if (unlink $File::Find::name) {
+          $self->logger->info("clean_files: removing non referenced file: ${File::Find::name}");
+        }
+        else {
+          $self->logger->log_and_croak(level => 'error', message => "Failed to remove file: ${File::Find::name}");
+        }
+      }
+    }
+  }, $base_dir);
+}
+sub add_files {
+  my $self = shift;
+}
+sub remove_files {
+  my $self = shift;
+}
+sub init {
+  my $self = shift;
+  #my $repodata_path = File::Spec->catdir($repo_dir, 'repodata');
+  #$self->app->logger->debug("init: repodata_path: ${repodata_path}");
+  #unless (-d $repodata_path) {
+  #  $self->app->logger->debug("init: make_path: ${repodata_path}");
+  #  my $make_path_error;
+  #  #my $dirs = File::Path->make_path($repodata_path, { error => \$make_path_error },);
+  #  #my $dirs = File::Path->make_path($repodata_path);
+  #  unless (File::Path->make_path($repodata_path)) {
+  #    $self->app->logger->log_and_croak(level => 'error', message => "init: unable to create path: ${repodata_path}");
+  #  }
+  #}
+#
+#  $self->_run_createrepo();
+}
+
+sub type {
+  #my $self = shift;
+  return 'Yum';
+}
+
+1;
